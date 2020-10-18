8143d3a86a58d78ead5c9b5ee561f28638637b36
luispurizaca <42756216+luispurizaca@users.noreply.github.com>
2020-10-17 21:41:18 -0500

    background

--- config/html2pdf/documentos/imprimir.php
+++ config/html2pdf/documentos/imprimir.php
@@ -77,7 +77,7 @@ if($tipo_plan == 1){
 <?php
 } else {
 ?>
-<table style="width: 100%; padding: 10px; border-collapse: collapse; background: url('../../../vendors/images/icono.png'); background-size: contain; background-position: center;">
+<table style="width: 100%; padding: 10px; border-collapse: collapse; background: url(../../../vendors/images/icono.png); background-size: contain; background-position: center;">
 <tr>
 <td style="width: 33.3%; color: #111; font-weight: bold; text-align: center; padding: 10px; font-size: 12px;">
 Inicio: <?php echo $fecha_plan; ?>
