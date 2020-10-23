34e85cd92c3fc669f9a63695adf4a783cd6bf217
luispurizaca <42756216+luispurizaca@users.noreply.github.com>
2020-10-23 11:33:28 -0500

    calendar

--- config/fullcalendar/index.php
+++ config/fullcalendar/index.php
@@ -52,39 +52,12 @@ $events = $req->fetchAll();
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Calendario</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">Menu</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
     <!-- Page Content -->
     <div class="container">
 
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h1>FullCalendar PHP MySQL</h1>
-                <p class="lead">Completa con rutas de archivo predefinidas que no tendrás que cambiar!</p>
+                <h1>Mi agenda</h1>
                 <div id="calendar" class="col-centered">
                 </div>
             </div>
