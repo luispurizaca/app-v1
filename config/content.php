285290340261c86f75aafbe0eef754527c4df003
luispurizaca <42756216+luispurizaca@users.noreply.github.com>
2020-11-01 12:33:36 -0500

    asc

--- config/content.php
+++ config/content.php
@@ -8,6 +8,165 @@ require_once (__DIR__.'/conexion_bd.php');
 //FORMULARIO NUEVO REGISTRO
 if($negocia_operacion == 1){
 
+//PDO
+require_once('fullcalendar/bdd.php');
+$sql = "SELECT id, title, start, end, color, id_nutricionista, id_paciente, id_suscripcion FROM events ";
+
+$req = $bdd->prepare($sql);
+$req->execute();
+
+$events = $req->fetchAll();
+?>
+<!-- Bootstrap Core CSS <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+
+<!-- FullCalendar -->
+<link href='css/fullcalendar.css' rel='stylesheet' />
+<style>
+#calendar {
+max-width: 800px;
+}
+.col-centered{
+float: none;
+margin: 0 auto;
+}
+</style>
+<!-- Modal ADD -->
+<div class="modal fade" id="ModalAdd" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<form class="form-horizontal" method="POST" action="addEvent.php">
+
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+<h4 class="modal-title" id="myModalLabel">Agregar Evento</h4>
+</div>
+<div class="modal-body">
+
+<div class="form-group">
+<label for="title" class="col-sm-2 control-label">Titulo</label>
+<div class="col-sm-10">
+<input type="text" name="title" class="form-control" id="title" placeholder="Titulo">
+</div>
+</div>
+<div class="form-group">
+<label for="color" class="col-sm-2 control-label">Color</label>
+<div class="col-sm-10">
+<select name="color" class="form-control" id="color">
+<option value="">Seleccionar</option>
+<option style="color:#0071c5;" value="#0071c5">&#9724; Azul oscuro</option>
+<option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquesa</option>
+<option style="color:#008000;" value="#008000">&#9724; Verde</option>						  
+<option style="color:#FFD700;" value="#FFD700">&#9724; Amarillo</option>
+<option style="color:#FF8C00;" value="#FF8C00">&#9724; Naranja</option>
+<option style="color:#FF0000;" value="#FF0000">&#9724; Rojo</option>
+<option style="color:#000;" value="#000">&#9724; Negro</option>
+
+</select>
+</div>
+</div>
+<div class="form-group">
+<label for="start" class="col-sm-2 control-label">Fecha Inicial</label>
+<div class="col-sm-10">
+<input type="text" name="start" class="form-control" id="start" readonly>
+</div>
+</div>
+<div class="form-group">
+<label for="end" class="col-sm-2 control-label">Fecha Final</label>
+<div class="col-sm-10">
+<input type="text" name="end" class="form-control" id="end" readonly>
+</div>
+</div>
+
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-info" data-dismiss="modal">Cerrar</button>
+<button type="submit" class="btn btn-primary">Guardar</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+<!-- Modal EDIT -->
+<div class="modal fade" id="ModalEdit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<form class="form-horizontal" method="POST" action="editEventTitle.php">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+<h4 class="modal-title" id="myModalLabel">Modificar Evento</h4>
+</div>
+<div class="modal-body">
+
+<div class="form-group">
+<label for="title" class="col-sm-2 control-label">Titulo</label>
+<div class="col-sm-10">
+<input type="text" name="title" class="form-control" id="title" placeholder="Titulo">
+</div>
+</div>
+<div class="form-group">
+<label for="color" class="col-sm-2 control-label">Color</label>
+<div class="col-sm-10">
+<select name="color" class="form-control" id="color">
+<option value="">Seleccionar</option>
+<option style="color:#0071c5;" value="#0071c5">&#9724; Azul oscuro</option>
+<option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquesa</option>
+<option style="color:#008000;" value="#008000">&#9724; Verde</option>						  
+<option style="color:#FFD700;" value="#FFD700">&#9724; Amarillo</option>
+<option style="color:#FF8C00;" value="#FF8C00">&#9724; Naranja</option>
+<option style="color:#FF0000;" value="#FF0000">&#9724; Rojo</option>
+<option style="color:#000;" value="#000">&#9724; Negro</option>
+
+</select>
+</div>
+</div>
+<div class="form-group"> 
+<div class="col-sm-offset-2 col-sm-10">
+<div class="checkbox">
+<label class="text-danger"><input type="checkbox"  name="delete"> Eliminar Evento</label>
+</div>
+</div>
+</div>
+
+<input type="hidden" name="id" class="form-control" id="id">
+
+
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+<button type="submit" class="btn btn-primary">Guardar</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+<!-- Modal OPEN -->
+<div class="modal fade" id="ModalOpen" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<form class="form-horizontal" method="POST" action="editEventTitle.php">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+<h4 class="modal-title" id="myModalLabel">Modificar Evento</h4>
+</div>
+<div class="modal-body">
+<div class="row">
+<div class="col-lg-12 text-center">
+<h1>Mi agenda</h1>
+<div id="calendar" class="col-centered">
+</div>
+</div>
+</div>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+<button type="submit" class="btn btn-primary">Guardar</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+<?php
+
 //TIPO DE USUARIO
 $id_paciente = (int)$_GET['negocia_tipo'];
 $get_nuevo_vendedor = (int)$_GET['get_nuevo_vendedor'];
@@ -321,7 +480,7 @@ $nombre_cb = $row_id_n[1];
 <div class="col-md-3 col-sm-6"></div>
 <div class="col-md-3 col-sm-6">
 <div class="form-group">
-<label class="n-label"><a href="config/fullcalendar/" target="_blank">Fecha de Inicio</a></label>
+<label class="n-label"><a href="javascript:void(0)" data-toggle="modal" data-target="#ModalOpen">Fecha de Inicio</a></label>
 <input id="form_fecha_suscripcion" name="form_fecha_suscripcion" class="form-control n-form-control" type="date" placeholder="Fecha de Inicio" value="<?php echo date('Y-m-d'); ?>">
 </div>
 </div>
